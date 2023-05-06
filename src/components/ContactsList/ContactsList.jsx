6f71ffccedabca3f4b9915e538fd50f4e397b3b4
Mariia Sarbieieva <sarbeeva.m.a@gmail.com>
2023-05-06 11:14:05 +0300

    test

--- src/components/ContactsList/ContactsList.jsx
+++ src/components/ContactsList/ContactsList.jsx
@@ -1,6 +1,6 @@
 import PropTypes from 'prop-types';
 import css from './ContactsList.module.css';
-
+// опопопооопоп
 export const ContactsList = ({ contacts, onClickButtonDelete }) => {
   return (
     <ul className={css.list}>
